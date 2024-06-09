e40e50dcb88bed4b4a02df36a5aa7ff73ddbd047
Jonny van Essen <jonny@sitefuel.io>
2024-06-09 19:29:20 -0400

    esketit

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from '../node_modules/firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,37 +21,6 @@ const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
 document.addEventListener('DOMContentLoaded', () => {
-  const resultDiv = document.getElementById('result');
-
-  const playGame = (choice) => {
-    const gameRef = database.ref('game');
-    gameRef.once('value').then((snapshot) => {
-      const data = snapshot.val();
-      const opponentChoice = data ? data.choice : null;
-
-      if (opponentChoice) {
-        gameRef.set(null); // Clear the game
-        const result = getResult(choice, opponentChoice);
-        resultDiv.innerText = `You: ${choice}, Opponent: ${opponentChoice}. Result: ${result}`;
-      } else {
-        gameRef.set({ choice });
-        resultDiv.innerText = 'Waiting for opponent...';
-      }
-    });
-  };
-
-  const getResult = (choice1, choice2) => {
-    if (choice1 === choice2) return 'Draw';
-    if (
-      (choice1 === 'rock' && choice2 === 'scissors') ||
-      (choice1 === 'paper' && choice2 === 'rock') ||
-      (choice1 === 'scissors' && choice2 === 'paper')
-    ) {
-      return 'You win!';
-    }
-    return 'You lose!';
-  };
-
   document.getElementById('rock').addEventListener('click', () => playGame('rock'));
   document.getElementById('paper').addEventListener('click', () => playGame('paper'));
   document.getElementById('scissors').addEventListener('click', () => playGame('scissors'));
